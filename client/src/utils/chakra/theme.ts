5b4e578f3764eed14adceb7fd700974bbea6f019
Oleksandr Tsvik <oleksandr.zwick@gmail.com>
2023-11-13 00:15:02 +0200

    fixed flashing theme change

--- client/src/utils/chakra/theme.ts
+++ client/src/utils/chakra/theme.ts
@@ -1,7 +1,18 @@
 import { extendTheme, type ThemeConfig } from '@chakra-ui/react';
 
+let initialColorMode: 'light' | 'dark' | 'system' = 'system';
+const chakraUiColorMode = localStorage.getItem('chakra-ui-color-mode');
+
+if (
+  chakraUiColorMode === 'light' ||
+  chakraUiColorMode === 'dark' ||
+  chakraUiColorMode === 'system'
+) {
+  initialColorMode = chakraUiColorMode;
+}
+
 const config: ThemeConfig = {
-  initialColorMode: 'system',
+  initialColorMode,
   useSystemColorMode: false,
 };
 
